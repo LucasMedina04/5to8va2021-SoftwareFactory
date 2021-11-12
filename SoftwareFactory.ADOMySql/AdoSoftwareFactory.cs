6acddce9011f92b249ba15a9f58c6bd1106dcb66
LucasMedina04 <medina.lucas.e@gmail.com>
2021-11-12 09:22:02 -0300

    Agregadas referencias y libreria Ado

---
+++ SoftwareFactory.ADOMySql/AdoSoftwareFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using SoftwareFactory.Core;
+using et12.edu.ar.AGBD.Ado;
+
+namespace SoftwareFactory.ADOMySql
+{
+    public class AdoSoftwareFactory
+    {
+        
+    }
+}
