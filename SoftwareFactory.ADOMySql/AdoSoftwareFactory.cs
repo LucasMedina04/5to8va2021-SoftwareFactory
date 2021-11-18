b61e525c09fe9850ceee31aaae99615c2a1c70e9
LucasMedina04 <medina.lucas.e@gmail.com>
2021-11-18 08:34:13 -0300

    Update AdoSoftwareFactory.cs

--- SoftwareFactory.ADOMySql/AdoSoftwareFactory.cs
+++ SoftwareFactory.ADOMySql/AdoSoftwareFactory.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using SoftwareFactory.Core;
 using et12.edu.ar.AGBD.Ado;
 
 namespace SoftwareFactory.ADOMySql
 {
     public class AdoSoftwareFactory
     {
-        
+        public AdoAGBD Ado { get; set; }
+        public AdoSoftwareFactory(AdoAGBD ado)
+        {
+            Ado = ado;
+        }
     }
 }
