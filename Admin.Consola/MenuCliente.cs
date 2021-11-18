b755394c3afbf705888db50ad6633fee22fc4f35
LucasMedina04 <medina.lucas.e@gmail.com>
2021-11-18 12:05:08 -0300

    me voy

--- Admin.Consola/MenuCliente.cs
+++ Admin.Consola/MenuCliente.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SoftwareFactory.ADOMySql;
+using SoftwareFactory.Core;
 
 namespace Admin.Consola
 {
-    class MenuCliente
+    static class MenuCliente
     {
+        public static void Iniciar(AdoSoftwareFactory Ado)
+        {
+            int Opcion;
+            do
+            {
+                Console.WriteLine("Menu de Tabla 'Cliente'\n" +
+                                  "0) Para salir\n" +
+                                  "1) Para dar de alta un cliente\n" +
+                                  "2) Para ver los clientes");
+                Opcion = int.Parse(Console.ReadLine());
+                switch (Opcion)
+                {
+                    case 1:
+                        Ado.AltaCliente(new Cliente(Cuit(), razonSocial));
+                        break;
+                    default:
+                        Console.WriteLine("Ingrese un número entre 0 y 2");
+                        break;
+                }
+            } while (Opcion != 0);
+        }
+        public static int Cuit()
+        {
+            int cuit;
+            do
+            {
+                Console.Write("Ingrese un cuit: ");
+            } while (int.TryParse(Console.ReadLine(), out cuit));
+            return cuit;
+        }
     }
 }
--- Admin.Consola/Program.cs
+++ Admin.Consola/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using et12.edu.ar.AGBD.Ado;
-using SoftwareFactory.ADO;
+using SoftwareFactory.ADOMySql;
 using SoftwareFactory.Core;
 
 namespace Admin.Consola
@@ -10,7 +10,28 @@ namespace Admin.Consola
         static void Main(string[] args)
         {
             var adoAGBD = FactoryAdoAGBD.GetAdoMySQL("appSettings.json", "test");
-            
+            var Ado = new AdoSoftwareFactory(adoAGBD);
+            int Opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Menú Principal\n" +
+                                  "0) Para salir\n" +
+                                  "1) Para acciones sobre Tabla 'Clientes'");
+                Opcion = int.Parse(Console.ReadLine());
+                switch (Opcion)
+                {
+                    case 1:
+                        MenuCliente.Iniciar(Ado);
+                    break;
+
+                    default:
+                        Console.WriteLine("Ingrese un valor entre 0 y (CantidadClases)");
+                        Console.ReadKey();
+                    break;
+                }
+
+            } while ();
         }
     }
 }
